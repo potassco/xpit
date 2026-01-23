9b90c3b968ae2b1b0bc12d2fe94a62ee8b78cd4c
Christoph Glinzer <christoph.glinzer@gmail.com>
2026-01-23 13:57:58 +0100

    __main__ for cli.

---
+++ src/xpit/__main__.py
@@ -0,0 +1,12 @@
+"""
+The main entry point for the application.
+"""
+
+
+def main() -> None:
+    """main function placeholder."""
+    print("xpit: CLI-tools coming soon!")
+
+
+if __name__ == "__main__":
+    main()
