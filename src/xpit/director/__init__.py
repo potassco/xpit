18beed4a60c0c6ef96a7b3303f6a86be5331f352
Orkunt Sabuncu <orkunt.sabuncu@potassco.com>
2026-04-02 09:59:41 +0200

    add ExplorerMethod and DistributionMethod importable

--- src/xpit/director/__init__.py
+++ src/xpit/director/__init__.py
@@ -3,7 +3,10 @@ Explanation director package
 """
 
 from .director import ExplanationDirector
+from .director import DistributionMethod, ExplorerMethod
 
 __all__ = [
     "ExplanationDirector",
+    "DistributionMethod",
+    "ExplorerMethod",
 ]
